bdf2d631ee4bed894632c450c07348aefa1bab85
Juuso Yli-Sorvari <izba.enophys@gmail.com>
2020-12-03 11:40:09 +0100

    removed old files

--- server/getMovie/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "../classes/server.php";
-header('Content-Type: application/json');
-
-$server = new Server();
-$server->handleTransactionMovie($_POST);
-$server->printResponse();
-
-?>
